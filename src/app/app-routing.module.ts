ebdbebfa17fc4d03fd67005b76f1b50d8f69787a
namsi-lia <lydianamsi@gmail.com>
2021-04-22 19:56:16 +0300

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { MainPageComponent } from './main-page/main-page.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {path:'',component:MainPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
